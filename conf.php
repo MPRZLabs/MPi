c35ebdb303d5a6513439c04543debe48e36f5bfe
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2013-10-09 21:11:08 +0000

    Imported license, readme and new config file

---
+++ conf.php
@@ -0,0 +1,11 @@
+<?php
+// Welcome to MPi configuration file. Just fill in the define fields.
+
+define("MPI_MYSQL_HOST", "");
+define("MPI_MYSQL_NAME", "");
+define("MPI_MYSQL_USER", "");
+define("MPI_MYSQL_PASS", "");
+define("MPI_MYSQL_PREF", "");
+define("MPI_SITE_TITLE", "");
+
+?>
