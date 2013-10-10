bf03222859745b7616abad3a3fa5f2bc9a0970f0
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2013-10-10 17:27:09 +0000

    Booting backends in conf.php

--- conf.php
+++ conf.php
@@ -3,16 +3,19 @@
   require("debug.php");
   // Welcome to MPi configuration file. Just fill in the fields.
   
-  
-  define("DATABASE_BACKEND", "mysql"); // can also be sqlite (not yet but soon)
+  //CHANGE STUFF HERE
+  define("DATABASE_BACKEND", "mysql");
 
   if (DATABASE_BACKEND == "mysql")
   {
     require("mysql.php");
+    // CHANGE STUFF HERE
+    $db = new DatabaseHandler("host", "user", "pass", "name");
   }
   elseif (DATABASE_BACKEND == "sqlite")
   {
     require("sqlite.php");
+    $db = new DatabaseHandler("system.sqlite");
   }
   else
   {
